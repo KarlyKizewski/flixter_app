7671eb27e4b59b0df87fa497198bffda175e8328
Karly Kizewski <kizewski.karly@gmail.com>
2019-08-22 19:23:57 +0000

    minimagick installed to resize to fill images for courses

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,7 +1,7 @@
 class ImageUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
+  include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
   # storage :file
@@ -30,7 +30,7 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
   # version :thumb do
-  #   process resize_to_fit: [50, 50]
+  process resize_to_fill: [800, 350]
   # end
 
   # Add a white list of extensions which are allowed to be uploaded.
