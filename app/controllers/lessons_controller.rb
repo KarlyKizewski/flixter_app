33aacb8a1898cbdeb4d2fe48d2e56cfeaf31a5ae
Karly Kizewski <kizewski.karly@gmail.com>
2019-08-23 16:19:59 +0000

    authentication for viewing lessons

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,12 +1,20 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_authorized_for_enrollment, only: [:show]
 
   def show
   end
 
   private
 
+  def require_authorized_for_enrollment
+    if current_lesson.section.course != current_user.enrolled_in?(current_lesson)
+      redirect_to courses_path, alert: 'You must be enrolled to view lesson.'
+    end
+  end
+
   helper_method :current_lesson
   def current_lesson
-    @current_lesson ||= Lesson.find(params[:id])
+    @current_lesson ||= Lesson.find(params[:id]) 
   end
 end
